5542333d799b4a25ba2d5a8a7262a600d78ceca1
Kyle T <khoavominh.truong.900@my.csun.edu>
2026-06-14 22:39:52 -0700

    Implemented the feature to shorten start up time for certain applications.

--- main.cpp
+++ main.cpp
@@ -6,42 +6,51 @@
 int main(int argc, char* argv[]) {
     if(argc < 2) {
         std::cout << "Welcome to FileManager, here are the options:" << "\n";
-        std::cout << "move: move the file to a new directory, location must be provided" <<"\n";
+        std::cout << "move: move the file to a new directory, filepath and new location must be provided" <<"\n";
         return 1;
     }
+    std::string command = argv[1];
 
-    if(argv[1] == "move") {
-        if(argv[2] == nullptr) {
-            std::cout << "file path must be provided" << "\n";
+    if(command == "move") {
+        if(argc < 3) {
+            std::cout << "location must be provided" << "\n";
             return 0;
         }
-        if(argv[3] == nullptr) {
-            std::cout << "location must be provided" << "\n";
+        std::string location = argv[3];
+    
+        if(argv[2] == "-latest") {
+            
         }
-        else {
-            std::filesystem::path filePath = argv[2];
-            if(argv[3] == "-re4r") {
-                std::filesystem::rename(filePath, R"(D:\Fluffy Mod Manager\Games\RE4R\Mods)");
-            }
-            if(argv[3] == "-sb") {
-                std::filesystem::rename(filePath, R"(D:\SteamLibrary\steamapps\common\StellarBlade\SB\Content\Paks\~mods\CustomNanosuitSystem)");
-            }
-            if(argv[3] == "re9") {
-                std::filesystem::rename(filePath, R"(D:\Fluffy Mod Manager\Games\RE9\Mods)");
-            }
+        std::filesystem::path filePath = argv[2];
+
+        if(location == "-re4r") {
+            std::filesystem::rename(filePath, R"(D:\Fluffy Mod Manager\Games\RE4R\Mods)");
+        }
+        if(location == "-sb") {
+            std::filesystem::rename(filePath, R"(D:\SteamLibrary\steamapps\common\StellarBlade\SB\Content\Paks\~mods\CustomNanosuitSystem)");
+        }
+        if(location == "re9") {
+            std::filesystem::rename(filePath, R"(D:\Fluffy Mod Manager\Games\RE9\Mods)");
+        }
+        if(location == "-bak") {
+            std::filesystem::rename(filePath, R"()");
         }
     }
     
-    if(argv[1] == "run") {
-        if(argv[2] == nullptr) {
-            std::cout << "provide a program to run" << "\n";
-            return 0;
-        }
-        if(argv[2] == "-picture") {
+    if(command == "run") {
+        std::string program = argv[2];
+
+        if(program == "-picture") {
             std::system(R"(explorer.exe C:\Users\kylet\Pictures)");
         }
-        if(argv[2] == "-user") {
+        if(program == "-user") {
             std::system(R"(explorer.exe C:\Users)");
+        }
+        if(program == "-desk") {
+            std::system(R"(explorer.exe C:\Users\kylet\Desktop)");
+        }
+        if(program =="-download") {
+            std::system(R"(explorer.exe C:\Users\kylet\Downloads)");
         }
     }
 }
