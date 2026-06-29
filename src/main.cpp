2835600c6b8dd2a0473638aadd8c86409d239a66
Kyle T <khoavominh.truong.900@my.csun.edu>
2026-06-28 19:20:01 -0700

    Added cli command to initiate moving files from one location to another.

--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,30 @@ int main(int argc, char* argv[]) {
     if(argc < 2) {
         std::cout << "Welcome to FileManager, here are the options: " << "\n";
         std::cout << "move: move the file to a new directory, location must be provided" << "\n";
-        return 1;
+        return 0;
     }
 
-    std::string command = argv[1];
-
-    if(command == "move") {
+    if(argv[1] == "move") {
         if(argc < 3) {
             std::cout << "location must be provided" << "\n";
             return 0;
         }
         else {
+            std::string originalPath = argv[2];
             std::string filePath = readJSON(argv[3]);
             if(filePath.empty()) {
+                return 1;
+            }
+            else {
+                moveFile(originalPath, filePath);
                 return 0;
             } 
+        }
+    }
+
+    if(argv[1] == "add") {
+        if(argc < 3) {
+            std::cout << "name and location must be provided" << "\n";
         }
     }
 
